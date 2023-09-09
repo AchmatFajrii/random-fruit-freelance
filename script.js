00f45a5ce912044927a1022dd0c05b3033c931ec
HeyFajri <achmatfajri@gmail.com>
2023-09-09 09:18:24 -0800

    ubah perjam

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+// const buahBuahan = [
+//   "Apel🍏",
+//   "Pisang🍌",
+//   "Jeruk🍊",
+//   "Mangga🥭",
+//   "Semangka🍉",
+//   "Melon🍈",
+//   "Anggur🍇",
+//   "Nanas🍍",
+// ];
+
+// const warnaBuahan = [
+//   "#D0E9D7", // Warna untuk Apel
+//   "#FFE135", // Warna untuk Pisang
+//   "#FFA500", // Warna untuk Jeruk
+//   "#FFD700", // Warna untuk Mangga
+//   "#FF3B3F", // Warna untuk Semangka
+//   "#98E2C6", // Warna untuk Melon
+//   "#4B0082", // Warna untuk Anggur
+//   "#FFD700", // Warna untuk Nanas
+// ];
+
+// function acakArray(seed, array) {
+//   const result = [...array];
+//   let m = result.length,
+//     t,
+//     i;
+
+//   while (m) {
+//     seed = (seed * 9301 + 49297) % 233280; // formula pseudo-random
+//     i = Math.floor((seed * m--) / 233280); // memastikan indeks adalah bilangan bulat
+//     t = result[m];
+//     result[m] = result[i];
+//     result[i] = t;
+//   }
+//   return result;
+// }
+
+// function tampilkanBuah(index) {
+//   const buahTerpilih = buahBuahan[index];
+//   const elemenBuah = document.querySelector("#buahTerpilih");
+//   elemenBuah.innerText = buahTerpilih;
+
+//   // Mengatur warna latar belakang sesuai dengan buah yang dipilih
+//   document.body.style.backgroundColor = warnaBuahan[index];
+// }
+
+// function tampilkanBuahBerdasarkanWaktu() {
+//   const waktuSaatIni = new Date();
+//   const seed = waktuSaatIni.getSeconds() / 60;
+//   const urutanAcak = acakArray(
+//     seed,
+//     Array.from({ length: buahBuahan.length }, (_, i) => i)
+//   );
+//   const indexBuah = urutanAcak[0];
+//   tampilkanBuah(indexBuah);
+// }
+
+// function aturTimerUntukDetikBerikutnya() {
+//   setTimeout(() => {
+//     tampilkanBuahBerdasarkanWaktu();
+//     aturTimerUntukDetikBerikutnya();
+//   }, 1000);
+// }
+
+// // Panggil fungsi berdasarkan detik saat halaman dimuat
+// tampilkanBuahBerdasarkanWaktu();
+// // Atur timer untuk memperbarui tampilan setiap detik
+// aturTimerUntukDetikBerikutnya();
+
+// PERJAM
+
 const buahBuahan = [
   "Apel🍏",
   "Pisang🍌",
@@ -47,7 +119,7 @@ function tampilkanBuah(index) {
 
 function tampilkanBuahBerdasarkanWaktu() {
   const waktuSaatIni = new Date();
-  const seed = waktuSaatIni.getSeconds() / 60;
+  const seed = waktuSaatIni.getHours();
   const urutanAcak = acakArray(
     seed,
     Array.from({ length: buahBuahan.length }, (_, i) => i)
@@ -56,14 +128,18 @@ function tampilkanBuahBerdasarkanWaktu() {
   tampilkanBuah(indexBuah);
 }
 
-function aturTimerUntukDetikBerikutnya() {
+function aturTimerUntukJamBerikutnya() {
+  const waktuSaatIni = new Date();
+  const milidetikHinggaJamBerikutnya =
+    (3600 - waktuSaatIni.getMinutes() * 60 - waktuSaatIni.getSeconds()) * 1000;
+
   setTimeout(() => {
     tampilkanBuahBerdasarkanWaktu();
-    aturTimerUntukDetikBerikutnya();
-  }, 1000);
+    aturTimerUntukJamBerikutnya();
+  }, milidetikHinggaJamBerikutnya);
 }
 
-// Panggil fungsi berdasarkan detik saat halaman dimuat
+// Panggil fungsi berdasarkan jam saat halaman dimuat
 tampilkanBuahBerdasarkanWaktu();
-// Atur timer untuk memperbarui tampilan setiap detik
-aturTimerUntukDetikBerikutnya();
+// Atur timer untuk memperbarui tampilan pada awal jam berikutnya
+aturTimerUntukJamBerikutnya();
